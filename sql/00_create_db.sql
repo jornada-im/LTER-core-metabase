a83c2ff9b9f89c94a0f02980c3339c91e44086ab
Gregory Maurer <gregmaurer@gmail.com>
2021-02-04 04:04:09 +0000

    change locale

--- sql/00_create_db.sql
+++ sql/00_create_db.sql
@@ -6,7 +6,7 @@ CREATE DATABASE lter_core_metabase
     WITH 
     OWNER = %db_owner%
     ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.UTF-8'
-    LC_CTYPE = 'en_US.UTF-8'
+    LC_COLLATE = 'C.UTF-8'
+    LC_CTYPE = 'C.UTF-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
